3b998975011ae87ebf5cf14b9cddfe0fc3358545
root <root@localhost.localdomain>
2019-09-28 14:45:12 +0545

    About page has been redesigned.

--- app/src/main/java/com/pathibharatechnology/smartkishan/about_us/AboutUsActivity.java
+++ app/src/main/java/com/pathibharatechnology/smartkishan/about_us/AboutUsActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.pathibharatechnology.smartkishan.MainDashboardActivity;
 import com.pathibharatechnology.smartkishan.R;
@@ -13,14 +14,20 @@ import com.pathibharatechnology.smartkishan.SupportActionBarInitializer;
 
 public class AboutUsActivity extends AppCompatActivity {
 
+    TextView aboutUsText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+        aboutUsText = findViewById(R.id.aboutUsId);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         SupportActionBarInitializer.setUpSupportActionBar(getSupportActionBar(), "About Us", true);
+
+        aboutUsText.setText("Smart Krishi is a application which includes goods and services offers related to the agriculture. When you start browsing, the app displays an assortment of new goods and services. Buy and sell any agricultural products easily within the app.\n \nSmart Krishi is a mobile marketplace for local buyers and sellers.\n\nBuy or Sell anything, easily post your item for sale by taking a photo.\n\nBrowse agricultural items for sale through app.\n\nMessage, call or request to contact you, buyers and sellers from within the app. \n\nIf you want more information please feel free to contact us at smartkisan7@gmail.com");
 
     }
 
@@ -40,3 +47,4 @@ public class AboutUsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
